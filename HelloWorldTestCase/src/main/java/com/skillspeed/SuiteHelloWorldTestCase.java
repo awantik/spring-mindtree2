a68b7b4962817c49547f57520bc805945de3d73d
awantik <awantikdas@gmail.com>
2016-10-27 01:37:48 +0530

    Junit

---
+++ HelloWorldTestCase/src/main/java/com/skillspeed/SuiteHelloWorldTestCase.java
@@ -0,0 +1,20 @@
+package com.skillspeed;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.skillspeed.HelloWorldTestCase;
+import com.skillspeed.ParametrizedHelloWorldTestCase;
+
+@RunWith(Suite.class)
+@SuiteClasses({ParametrizedHelloWorldTestCase.class,HelloWorldTestCase.class})
+public class SuiteHelloWorldTestCase {
+	@Test
+	public void testSuite(){
+		assertEquals("Suite","Suite");
+	}
+}
